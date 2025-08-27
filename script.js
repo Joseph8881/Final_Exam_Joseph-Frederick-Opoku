217b0b11d273626169e20377bf0c3cc839ee1541
Joseph8881 <fredpoku88@gmail.com>
2025-08-27 14:05:57 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let greeting = "Hello, JavaScript!"; // String
+let temperature = 25; // Number
+
+
+const PI = 3.14159; // Number
+const isLoggedIn = true; // Boolean
+
+
+var userName = "Coder"; // String
+var userAge = null; // Null
+
+
+let unassignedVariable; // Undefined
+
+
+console.log("Using let:");
+console.log("Greeting:", greeting);
+console.log("Temperature:", temperature);
+
+console.log("Using const:");
+console.log("Value of PI:", PI);
+console.log("User is logged in:", isLoggedIn);
+
+console.log("Using var:");
+console.log("User Name:", userName);
+console.log("User Age:", userAge);
+
+console.log("Undefined variable:");
+console.log("Unassigned Variable:", unassignedVariable);
